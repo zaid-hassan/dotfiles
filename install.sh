29a5d736e20316bdf2f7d4a48a41c416e7217a45
zaid-hassan <zaidhassan251@gmail.com>
2023-07-10 22:32:45 +0530

    made a script to move i3 config and added reflector to package installation list

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # 1. Install Packages using pacman
 #
 sudo pacman -Syu # System upgrade
-sudo pacman -S brightnessctl polybar picom nitrogen maim starship alacritty
+sudo pacman -S brightnessctl polybar picom nitrogen maim starship alacritty reflecotor
 
 # 1.1 Install AUR Packages using paru
 #
---
+++ movei3.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+#
+# Make a copy of stock conf
+cp $HOME/.config/i3/conf $HOME/.config/i3/conf.stock
+
+# Move my i3 conf to i3 directory
+cp $HOME/dotfiles/i3/conf $HOME/.config/i3/conf
